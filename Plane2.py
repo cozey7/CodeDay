095e4e602a332536ff38873494aa5d77abc45c69
CHH150 <87557988+CHH150@users.noreply.github.com>
2021-11-12 21:26:16 -0800

    update_v.1

---
+++ Plane2.py
@@ -0,0 +1,13 @@
+
+
+import keyboard
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            print('You pressed a key')
+            break
+    except:
+        break
+#def button():
+ #   if 
+#def planeshape():
