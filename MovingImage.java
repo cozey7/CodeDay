d8cddab95bfb6d55376d97e3e67b3983289f34fd
cz0713 <56445917+cz0713@users.noreply.github.com>
2021-11-13 14:44:00 -0800

    Checkpoint

---
+++ MovingImage.java
@@ -0,0 +1,28 @@
+import java.awt.geom.Rectangle2D;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class MovingImage extends Rectangle2D.Double{
+
+    private PImage image;
+
+    public MovingImage(PImage img, double x, double y, double width, double height){
+        super(x, y, width, height);
+        image = img;
+    }
+
+    public void moveToLocation(double x, double y){
+        super.x = x;
+        super.y = y;
+    }
+
+    public void moveByAmount(double x, double y) {
+		super.x += x;
+		super.y += y;
+	}
+
+    public void draw(PApplet g){
+        g.image(image, (int)x, (int)y, (int)width, (int)height);
+    }
+
+}
--- Plane.java
+++ Plane.java
@@ -1,3 +1,13 @@
-public class Plane{
-    
+import processing.core.PImage;
+
+public class Plane extends MovingImage{
+
+    public Plane(PImage img, double x, double y, double width, double height) {
+        super(img, x, y, width, height);
+    }
+
+    public void fly(){
+        
+    }
+
 }
