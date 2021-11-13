942dcb807a44fe877ad44eeb3c37017c82524937
cz0713 <56445917+cz0713@users.noreply.github.com>
2021-11-13 12:56:04 -0800

    starting over

--- DrawingSurface.java
+++ DrawingSurface.java
@@ -1,20 +1,3 @@
-import java.awt.Canvas;
-import java.awt.Graphics;
-import javax.swing.JFrame;
-
-public class DrawingSurface extends Canvas{
-    public static void main(String [] args) {
-       JFrame frame = new JFrame("My Drawing");
-       Canvas canvas = new DrawingSurface();
-       canvas.setSize(400, 400);
-       frame.add(canvas);
-       frame.pack();
-       frame.setVisible(true);
-
-       Plane plane1 = new Plane(100, 100);
-    }
-
-    public void paint(Graphics g){
-        g.fillOval(100, 100, 200, 200);
-    }
+public class DrawingSurface{
+    
 }
--- Plane.java
+++ Plane.java
@@ -1,9 +1,3 @@
-
-public class Plane {
-    private int x, y;
-
-    public Plane(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
+public class Plane{
+    
 }
