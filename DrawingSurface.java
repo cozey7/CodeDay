46f6735c52767d20dd521318f0f9d055f0fd4229
cz0713 <56445917+cz0713@users.noreply.github.com>
2021-11-13 23:01:40 -0800

    randomize projectile every round, haven't removed projectile when touched yet

--- DrawingSurface.java
+++ DrawingSurface.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.lang.Math;
 import java.awt.geom.Rectangle2D;
 import processing.core.PApplet;
 import processing.core.PImage;
@@ -9,16 +9,17 @@ public class DrawingSurface extends PApplet {
     private PImage redPlaneImg, bluePlaneImg, groundImg, powerupImg, bulletImg, explosionImg;
     private Plane bluePlane, redPlane;
     private ArrayList<Rectangle2D.Double> objects;
-    private Powerup pu1;
+    private Powerup pu1, pu2;
     private Ground ground;
     private Sky sky;
     private int victoryTimer, scoreRed, scoreBlue, blueShots, redShots;
+    private int spawnTimer;
 
     public void setup(){
         bluePlaneImg = loadImage("bluePlane.png");
         redPlaneImg = loadImage("redPlane.png");
         groundImg = loadImage("ground.png");
-        powerupImg = loadImage("powerup.png");
+        powerupImg = loadImage("powerup1.png");
         bulletImg = loadImage("bullet.png");
         explosionImg = loadImage("explosion.png");
         bluePlane = new Plane(bluePlaneImg, explosionImg, 100, 100, 50, 50);
@@ -32,12 +33,15 @@ public class DrawingSurface extends PApplet {
         objects.add(redPlane);
         objects.add(ground);
         objects.add(sky);
-        pu1 = new Powerup(powerupImg, 100, 200, 20, 20);
+        pu1 = new Powerup(powerupImg, 30, 200, 20, 20);
+        //pu2 = new Powerup(powerupImg, -50, -50, 20, 20);
         objects.add(pu1);
+        //objects.add(pu2);
         blueShots = 0;
         redShots = 0;
         scoreRed = 0;
         scoreBlue = 0;
+        spawnTimer = 0;
     }
     public DrawingSurface(){
         
@@ -94,12 +98,27 @@ public class DrawingSurface extends PApplet {
                 objects.add(redPlane);
                 objects.add(ground);
                 objects.add(sky);
+                pu1 = spawnPowerup();
+                objects.add(pu1);
                 victoryTimer = 0;
             }
         }
         textSize(100);
         textAlign(CENTER);
         text(scoreBlue + "         " + scoreRed, 400, 500);
+
+        // if(spawnTimer > 1000){
+        //     if(pu1.getX() < 0){
+        //         pu1 = spawnPowerup();
+        //         pu1.draw(this);
+        //     } else if(pu2.getX() < 0){
+        //         pu2 = spawnPowerup();
+        //         objects.add(pu2);
+        //         pu2.draw(this);
+        //     } else 
+        //         spawnTimer = 0;
+        // }
+        spawnTimer++;
     }
     public void settings() {
         size(800, 600);
@@ -115,5 +134,19 @@ public class DrawingSurface extends PApplet {
             bluePlane.setFlying(false);
         else if (keyCode == UP && !redPlane.isCrashing())
             redPlane.setFlying(false);
+    }
+
+    public Powerup spawnPowerup(){
+        int x, y;
+        int a = (int)(2*Math.random());
+        if(a == 0){
+            x = 30;
+        } else {
+            x = 750;
+        }
+        int b = (int)(350*Math.random());
+        y = 50 + b;
+
+        return new Powerup(powerupImg, x, y, 20, 20);
     }
 }
