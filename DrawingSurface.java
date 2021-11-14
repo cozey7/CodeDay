9b6d36d3a7a0a8f4d30e6a0b99b0532719e43a3f
Michael Chen <38442638+electriql@users.noreply.github.com>
2021-11-13 17:22:53 -0800

    plane collision

--- DrawingSurface.java
+++ DrawingSurface.java
@@ -4,13 +4,14 @@ import processing.core.PImage;
 public class DrawingSurface extends PApplet {
 
     PImage plane;
-    private Plane bluePlane;
-    private boolean p1Fly, p2Fly;
+    private Plane bluePlane, redPlane;
 
     public void setup(){
         plane = loadImage("plane.png");
         bluePlane = new Plane(plane, 100, 100, 50, 50);
-        bluePlane.setVx(-4);
+        bluePlane.setVx(2);
+        redPlane = new Plane(plane, 600, 100, 50, 50);
+        redPlane.setVx(-2);
     }
     public DrawingSurface(){
         
@@ -19,18 +20,23 @@ public class DrawingSurface extends PApplet {
     public void draw() {
         background(130, 199, 255);
         bluePlane.draw(this);
+        redPlane.draw(this);
+        bluePlane.collide(redPlane);
+        redPlane.collide(bluePlane);
     }
     public void settings() {
         size(800, 600);
     }
     public void keyPressed() {
-        if (key == 'w' || key == 'W')
+        if ((key == 'w' || key == 'W') && !bluePlane.isCrashing())
             bluePlane.setFlying(true);
-        else if (keyCode == UP);
+        else if (keyCode == UP && !redPlane.isCrashing())
+            redPlane.setFlying(true);
     }
     public void keyReleased() {
-        if (key == 'w' || key == 'W')
+        if ((key == 'w' || key == 'W') && !bluePlane.isCrashing())
             bluePlane.setFlying(false);
-        else if (keyCode == UP);
+        else if (keyCode == UP && !redPlane.isCrashing())
+            redPlane.setFlying(false);
     }
 }
--- Plane.java
+++ Plane.java
@@ -6,33 +6,46 @@ public class Plane extends MovingImage{
 
     private final double GRAVITY = 0.2;
     private double xVel, yVel;
-    private boolean flying;
+    private boolean flying, crashing, dead;
 
     public Plane(PImage img, double x, double y, double width, double height) {
         super(img, x, y, width, height);
         xVel = 0;
         yVel = 0;
+        crashing = false;
     }
 
-    private void act(PApplet g){
+    private void act(PApplet g) {
         if (flying && yVel >= -4)
             yVel -= 1;
         else
             yVel += GRAVITY;
-        moveByAmount(xVel, yVel);
+        if (!dead)
+            moveByAmount(xVel, yVel);
         if (x > g.width)
             moveToLocation(-width, y);
         else if (x + width < 0)
             moveToLocation(g.width, y);
     }
     public boolean collide(Rectangle2D.Double image) {
-        if (this.intersects(image)) {
+        Rectangle2D.Double hitbox = new Rectangle2D.Double(x + xVel, y + yVel, width, height);
+        if (hitbox.intersects(image)) {
             if (image instanceof Plane) {
-                if (y > image.getY() + image.getHeight() * 2.0/3)
+                if (y > image.getY() + image.getHeight() * 2.0/3) {
+                    yVel = -yVel;
+                    crashing = true;
+                    flying = false;
                     return true;
-                // else swap directions
+                }
+                else if (y + image.getHeight() * 2.0/3 < image.getY())
+                    yVel = -yVel;
+                else
+                    xVel = -xVel;
             }
-            return image instanceof Ground;
+            if (image instanceof Ground) {
+                dead = true;
+                return true;
+            }
         }
         return false;
     }
@@ -45,7 +58,9 @@ public class Plane extends MovingImage{
     public void setVy(double yVel) {
         this.yVel = yVel;
     }
-
+    public boolean isCrashing() {
+        return crashing;
+    }
     public void draw(PApplet g) {
         act(g);
         super.draw(g);
