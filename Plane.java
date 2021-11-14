ccb3d13096b07776730dc5518895d64a19bdd95f
Michael Chen <38442638+electriql@users.noreply.github.com>
2021-11-13 16:08:35 -0800

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -1,31 +1,38 @@
+import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Plane extends MovingImage{
 
-    private double gravity;
+    private final double GRAVITY = 0.98;
     private double xVel, yVel;
 
     public Plane(PImage img, double x, double y, double width, double height) {
         super(img, x, y, width, height);
-        gravity = -9.8;
         xVel = 0;
         yVel = 0;
     }
 
-    public void fly(){
-        yVel += 0.1;
-    }
-
-    public void fall(){
-
-    }
-
-    public void act(){
-        double xCoord = getX();
-
-        yVel += gravity;
-        double yCoord = getY() + yVel;
+    private void act(PApplet g){
+        yVel += GRAVITY;
+        moveByAmount(xVel, yVel);
+        if (x > g.displayWidth)
+            moveToLocation(-width, y);
+        else if (x - width < 0)
+            moveToLocation(g.displayWidth, y);
 
     }
+    public boolean collide(MovingImage image) {
+        return false;
+    }
+    public void setVx(double xVel) {
+        this.xVel = xVel;
+    }
+    public void setVy(double yVel) {
+        this.yVel = yVel;
+    }
 
+    public void draw(PApplet g) {
+        act(g);
+        super.draw(g);
+    }
 }
