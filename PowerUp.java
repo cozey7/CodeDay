a18e34b324487614d31cdb794c733e3fa76a27ff
cz0713 <56445917+cz0713@users.noreply.github.com>
2024-01-25 00:04:52 -0500

    Update PowerUp.java

--- PowerUp.java
+++ PowerUp.java
@@ -4,6 +4,7 @@ public class Powerup extends MovingImage{
 
     public Powerup(PImage img, double x, double y, double width, double height) {
         super(img, x, y, width, height);
+        
     }
     
 }
